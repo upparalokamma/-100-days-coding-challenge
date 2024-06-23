29bd182927690f2a7eeea6b968e502b048e1b85b
upparalokamma <163702956+upparalokamma@users.noreply.github.com>
2024-06-23 23:03:59 +0530

    Create Day7_palindrome.java

---
+++ Day7_palindrome.java
@@ -0,0 +1,70 @@
+//PALINDROME STRING
+
+
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        // String str="MALAYALAM";
+        int len=str.length();
+        int start=0,end=len-1;
+        
+        // System.out.println(start+" "+end);
+        //  System.out.println(str.charAt(start)+" "+str.charAt(end));
+         
+        //  for(;str.charAt(start)==str.charAt(end) && start<=end;)
+        //  while(str.charAt(start)==str.charAt(end) && start<=end)
+        {
+        //   System.out.println(start+" "+end);
+        //   System.out.println(str.charAt(start)+" "+str.charAt(end));
+            start++;
+            end--;
+        // System.out.println(start+" "+end);
+        // System.out.println(str.charAt(start)+" "+str.charAt(end));
+        
+        }
+        
+        if(str.charAt(start)==str.charAt(end))
+        {
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+        
+        //  System.out.println(start+" "+end);
+        // System.out.println(str.charAt(start)+" "+str.charAt(end));
+      
+    }
+}
+//malayalam
+
+
+
+
+//PALINDROME NUMBER
+
+
+public class Main{
+    public static void main(String[] args){
+        int sum=0,temp;
+        int n=141;
+        temp=n;
+        while(n>0)
+        {
+            int r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }System.out.println(sum);
+        
+        if(temp==sum)
+        {
+            System.out.println("is prime");
+        }
+        else{
+            System.out.println("is not a prime");
+        }
+    }
+}
