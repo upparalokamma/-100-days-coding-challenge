82cc7c41901a9a184f6a90d1689803dc36b20587
upparalokamma <163702956+upparalokamma@users.noreply.github.com>
2024-06-26 22:55:56 +0530

    Update day10_patient readmission prediction.py

--- day10_patient readmission prediction.py
+++ day10_patient readmission prediction.py
@@ -1,3 +1,3 @@
-#i worked on doing my internship project here is the code 
+#i worked on my internship project here is the code 
 
 https://colab.research.google.com/drive/1wu4Hb46jA7IxHy8dKWrnqogg8HBt--K2?usp=sharing
