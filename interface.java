08b1395908f4741d08a2b24c1d64010a6c277e0b
upparalokamma <163702956+upparalokamma@users.noreply.github.com>
2024-09-19 23:51:46 +0530

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,10 @@
+What is a class, and how does it differ from an interface in Java?
+Can a class implement multiple interfaces? Why or why not?
+What are the key differences between method declarations in a class and in an interface?
+Can an interface have instance variables? Explain your answer.
+How do access modifiers (like public, private, etc.) differ between classes and interfaces?
+Can a class extend multiple classes? How does this differ from implementing interfaces?
+What is the purpose of a default method in an interface, and how is it different from methods in a class?
+When would you use an abstract class instead of an interface?
+Can an interface extend another interface? If yes, what happens to the methods in the extended interface?
+What is the difference between creating an object of a class and using an interface as a reference?
